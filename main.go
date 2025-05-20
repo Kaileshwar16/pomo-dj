5f1c60cc22fccc1b4e4791ebd08fd192f8aaa757
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-05-20 21:38:06 +0530

    Finnaly made websocket work

--- main.go
+++ main.go
@@ -1,16 +1,123 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+	hub  *Hub
+}
+
+type Hub struct {
+	clients map[*Client]bool
+	lock    sync.Mutex
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients: make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) addClient(c *Client) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	h.clients[c] = true
+}
+
+func (h *Hub) removeClient(c *Client) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	if _, ok := h.clients[c]; ok {
+		delete(h.clients, c)
+		close(c.send)
+	}
+}
+func (h *Hub) broadcast(message []byte) {
+	h.lock.Lock()
+	clients := make([]*Client, 0, len(h.clients))
+	for c := range h.clients {
+		clients = append(clients, c)
+	}
+	h.lock.Unlock()
+
+	for _, client := range clients {
+		select {
+		case client.send <- message:
+		default:
+			h.removeClient(client)
+		}
+	}
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.removeClient(c)
+		c.conn.Close()
+	}()
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+		log.Printf("Received: %s", message)
+		c.hub.broadcast(message)
+	}
+}
+
+func (c *Client) writePump() {
+	for msg := range c.send {
+		err := c.conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+	c.conn.Close()
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/static", "./static/")
+	r.LoadHTMLFiles("templates/index.html")
+	r.Static("/static", "./static")
+
+	hub := newHub()
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	r.Run("0.0.0.0:8080")
+
+	r.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println("Upgrade error:", err)
+			return
+		}
+
+		client := &Client{
+			conn: conn,
+			send: make(chan []byte, 256), // buffered channel for outgoing messages
+			hub:  hub,
+		}
+
+		hub.addClient(client)
+
+		go client.writePump()
+		client.readPump()
+	})
+
+	r.Run(":8080")
 }
